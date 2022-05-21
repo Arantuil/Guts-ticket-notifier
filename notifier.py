4a97fce0f0145b707b67e0054994d42d4790f6b7
Arantuil <gideon.t.v.d.k@hotmail.com>
2022-05-21 21:08:09 +0200

    gmail notifications added

--- notifier.py
+++ notifier.py
@@ -1,8 +1,34 @@
-import selenium
+from selenium import webdriver
+import time
 
 import smtplib
 from personaldata import *
 
-server = smtplib.SMTP
-server.starttls()
-server.login(f'{test}')
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(emailaddress, emailpassword)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(emailaddress, emailaddress, message)
+        server.quit()
+        print("Email sent.")
+    except:
+        print("Email failed to send.")
+
+PATH = "C:\Windows\System32\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://guts.events/m3pgrw/f215lz/resale")
+
+for i in range(100000):
+    time.sleep(5)
+    page = driver.page_source
+    word = "Vrijdag"
+    if (word in page) == True:
+        print("Ticket gevonden!")
+        send_email('Ticket gevonden!', 'Er is een ticket gevonden voor vrijdag!')
+    else:
+        print('[]')
+    driver.refresh()
+
+driver.quit()
